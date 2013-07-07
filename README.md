e2eb71b4294de1833ac289e9ed2331110b6cd61a
GurudayalKhalsa <gurudayalkhalsa@gmail.com>
2013-07-07 11:08:40 -0400

    added documentatiion, prepended and appended inputs and more

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-CSS-Framework
+Base CSS Framework
 =============
 
 A lightweight Responsive CSS Framework
@@ -9,11 +9,15 @@ See demo folder or https://dl.dropboxusercontent.com/u/103707041/github/Base%20C
 
 ###Features:
 
-- supports all browsers from ie8+
-- no js dependencies for default features (jQuery needde for click dropdowns, tooltips etc.)
+- Inspired by Bootstrap, Skeleton, Pure, Normalize.css and others
+- supports all main browsers from ie8+
+- no js dependencies for default features (jQuery needed  for tooltips, button effects etc.)
 - includes quite a few polyfills from html5 placeholders to media queries for ie
-- small compared to many other frameworks
 - 16 column grid system
+- six buttons with automatic new button hover and active colors
+- tables
+- 
+- small compared to many other frameworks
 - responsive
 
 ###Files to include in html: 
@@ -22,6 +26,261 @@ See demo folder or https://dl.dropboxusercontent.com/u/103707041/github/Base%20C
 
 ###Optional: 
 
-- Fontawesome
+- Font Awesome for icons
 
-Licensed under the MIT License - http://opensource.org/licenses/MIT
+#####Licensed under the MIT License - http://opensource.org/licenses/MIT
+
+
+=============
+
+#Documentation:
+
+##Navigation:
+
+####Navigation Menus
+######To make a nav, use the "nav" class on a ul element.
+Here is some example HTML for creating a navigation menu
+
+	<ul class="nav">
+		<li><a href="home">Home</a></li>
+		<li><a href="services">Services</a></li>
+		<li><a href="about">About</a></li>
+		<li><a href="contact">Contact</a></li>
+	</ul>
+	
+The above nav will be a horizontal menu.
+
+####Dropdowns
+
+To make a dropdown menu, use the class "dropdown" on an element (usually an li element inside a ul), and create a ul as a child of that element.
+
+	<ul class="nav">
+		<li><a href="home">Home</a></li>
+		<li class="dropdown nocaret click"><a href="services">Services</a>
+			<ul>
+				<li><a href="home">Home</a></li>
+				<li><a href="services">Services</a></li>
+				<li><a href="about">About</a></li>
+				<li><a href="contact">Contact</a></li>
+			</ul>
+		</li>
+		<li><a href="about">About</a></li>
+		<li><a href="contact">Contact</a></li>
+	</ul>
+	
+
+- By default, the parent of a dropdown menu (in this case the li) will have a caret to the right of the text. You can disable this by giving that element the class "nocaret". Additionally, you can give any element a caret by giving that element the class "caret".	
+	
+- Dropdown menus will by default open up when their parent is hovered over. This requires no js.
+
+- Optionally, if a classname of "click" is also specified on the parent element, the dropdown menu will only drop down when the parent is clicked and not on hover. Note that this requires javascript (does not require jQuery). By require I mean javascript is used to perform this task.
+
+####Main Menu/Header
+
+An example header wold look like this:
+
+    <div class="header">
+      <div class="container">
+        <a href="#" class="logo">Some Website</a>
+        <a class="navdrop"></a>
+        
+        <ul class="nav">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>   
+          <li class="dropdown"><a href="#">Portfolio</a>
+            <ul>
+              <li><a href="#">Art</a></li>
+              <li><a href="#">Websites</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+        
+      </div>
+    </div>  
+
+- An item with classname "header" must be present to create what appears on the demo page (where the nav is aligned right and logo aligned left)
+
+- an item with classname "container" centers and makes element fixed width (responsive), normally 960px, getting smaller as window is resized
+
+- The element with class "navdrop" is displayed when small window viewport size, with the nav hidden until the navdrop is pressed. This all requires javascript (does not require jQuery)
+
+##Buttons
+
+	<button class="button button-blue">A blue button</button>
+	<button class="button button-green">A green button</button>
+	<button class="button button-red">A red button</button>
+	<button class="button button-orange">An orange button</button>
+	<button class="button button-cyan">A cyan button</button>
+	<button class="button">A Regular button</button>
+	
+- An element is styled like a button with the class "button". This classname must be present
+- More than just <button>Button</button> elements can be styled with the class "button"
+
+New button styles can be easily made with css, only declaring: 
+	
+    .button-mybutton{
+    	background-color: #08c;
+    }
+    
+- Will create a new button style with automatic :hover and :focus styles
+
+The markup for the above button could be:
+
+	<a class="button .button-mybutton">Button</a>
+	
+##Tables
+
+	<table class="table table-striped table-hover table-bordered">
+     <tbody>
+      <tr>
+        <td>8:30am-9:30am</td>
+        <td>Go for a jog</td>
+        <td>Central Park</td>
+      </tr>
+      <tr>
+        <td>10:00am-11:15am</td>
+        <td>Eat Breakfast</td>
+        <td>A place</td>
+      </tr>
+      <tr>
+        <td>4:30pm-5:30pm</td>
+        <td>Skype chat</td>
+        <td>Denise</td>
+      </tr>
+      <tr>
+        <td>6:00pm-7:15pm</td>
+        <td>Eat dinner</td>
+        <td>Home</td>
+      </tr>
+     </tbody>
+    </table>
+    
+- .table class is needed
+- .table-striped adds zebra striping
+- .table-bordered creates a border around table, and vertical borders in between columns
+- .table-hover changes background color of row when hovered
+	
+##Grid
+
+- Responsive, 16-column grid
+- Class syntax for width of column is .one.column, .two.columns, .three.columns, etc. 
+- .one will make a box one 16th of the width of the container it is in, .two one eighth and so on
+- Optional .one-third.column and .two-thirds.column for one third and two thirds of the container, respectively
+- You may need to clearfix the parent div with .clearfix
+
+Example HTML:
+
+	<div id="products" class="clearfix">
+
+        <h4 class="products-title">Check Out Some Products</h4>
+
+        <div class="one-third column">
+            <img src="http://placehold.it/250x150">
+            <h5>This is a product</h5>
+        </div>
+
+        <div class="one-third column">
+            <img src="http://placehold.it/250x150">
+            <h5>This is another product</h5>
+        </div>
+
+        <div class="one-third column">
+            <img src="http://placehold.it/250x150">
+            <h5>This is yet another product</h5>
+        </div>
+    </div>
+    
+This will create three boxes beside eachother inside of its container
+
+##Forms
+
+	<form class="four columns">
+        <h4>This is a form:</h4>
+        <input type="text" placeholder="Enter your name.." />
+        <input type="radio" name="sex" value="male"><span>Male</span>
+        <input type="radio" name="sex" value="female"><span>Female</span><br/><br/>
+        <textarea placeholder="This is a text area."></textarea>
+        <input type="text" placeholder="This is a text field." />
+        <select>
+            <option>Cars</option>
+            <option>Bikes</option>
+            <option>Boats</option>
+            <option>Planes</option>
+            <option>Scooters</option>
+            <input type="submit" class="button" />
+        </select>
+    </form>
+    
+The above will create a form one quarter of the container it is in
+
+####Prepended and Appended Input
+
+- If you want to put a span or button before a <input type="text" /> field, put the two items in order inside an element with class-name .input-prepend
+- For after a <input type="text" />, put the two items in order inside a .input-append element.
+- You can have both prepended and appended inputs, just make the parent element be .input-prepend.input-append
+- The prepended and appended elements must be either a span or .button, and the input must be input[type="text"]
+
+#####Examples:	
+
+	<div class="input-append">
+        <input type="text" placeholder="Write some text" />
+        <button class="button button-green"><i class="icon-thumbs-up"></i></button>
+    </div> 
+
+	<div class="input-append input-prepend">
+        <span>Write</span>
+        <input type="text" placeholder="Write some text" />
+        <li class="button dropdown click"><span>More</span>
+            <ul>
+                <li><a><i class="icon-edit"></i>Edit</a></li>
+                <li><a><i class="icon-archive"></i>Compress</a></li>
+                <li><a><i class="icon-bookmark"></i>Bookmark</a></li>
+            </ul>
+        </li>
+    </div> 
+
+##Button-Groups
+
+	<ul class="button-group">
+        <li class="button"><i class="icon-download-alt"></i></li>
+        <li class="button"><i class="icon-expand"></i></li>
+        <li class="button"><i class="icon-info"></i></li>
+        <li class="button"><i class="icon-trash"></i></li>
+
+        <li class="button dropdown click" data-tooltip="Oogadaboogada woogadashoogada">More
+            <ul>
+                <li><a><i class="icon-edit"></i>Edit</a></li>
+                <li><a><i class="icon-archive"></i>Compress</a></li>
+                <li><a><i class="icon-bookmark"></i>Bookmark</a></li>
+            </ul>
+        </li>
+    </ul>
+    
+    <div class="button-group" data-toggle='button-radio'>
+        <a class="button" ><i class="icon-align-left"></i></a>
+        <a class="button"><i class="icon-align-center"></i></a>
+        <a class="button"><i class="icon-align-right"></i></a>
+    </div> 
+    
+- Parent must have class .button-group
+- Children must be .buttons  
+
+##Javascript Effects
+
+###Button Effects
+
+- A button group with data-toggle='button-radio''s children will have a radio button like behaviour, and .active when selected
+
+Example:
+
+	<div class="button-group" data-toggle='button-radio'>
+	    <a class="button" ><i class="icon-align-left"></i></a>
+	    <a class="button"><i class="icon-align-center"></i></a>
+	    <a class="button"><i class="icon-align-right"></i></a>
+	</div> 
+
+
+###Tooltips
+
+Any element with data-tooltip="mytooltip" will, on hover, make a tooltip appear under it with mytooltip as the tooltip
--- js/base.js
+++ js/base.js
@@ -1,21 +1,26 @@
 //get element css selector
-function getElem(e){function f(){if(navigator.userAgent.indexOf("MSIE 8")==-1&&navigator.userAgent.indexOf("MSIE 7")==-1&&navigator.userAgent.indexOf("MSIE 6")==-1)return!1;if(navigator.userAgent.indexOf("MSIE 8")>-1)return!0;if(navigator.userAgent.indexOf("MSIE 7")>-1)return!0;if(navigator.userAgent.indexOf("MSIE 6")>-1)return!0}var t=function(e,n,r){document.getElementsByClassName?t=function(e,t,n){n=n||document;var r=n.getElementsByClassName(e),i=t?new RegExp("\\b"+t+"\\b","i"):null,s=[],o;for(var u=0,a=r.length;u<a;u+=1){o=r[u];(!i||i.test(o.nodeName))&&s.push(o)}return s}:document.evaluate?t=function(e,t,n){t=t||"*";n=n||document;var r=e.split(" "),i="",s="http://www.w3.org/1999/xhtml",o=document.documentElement.namespaceURI===s?s:null,u=[],a,f;for(var l=0,c=r.length;l<c;l+=1)i+="[contains(concat(' ', @class, ' '), ' "+r[l]+" ')]";try{a=document.evaluate(".//"+t+i,n,o,0,null)}catch(h){a=document.evaluate(".//"+t+i,n,null,0,null)}while(f=a.iterateNext())u.push(f);return u}:t=function(e,t,n){t=t||"*";n=n||document;var r=e.split(" "),i=[],s=t==="*"&&n.all?n.all:n.getElementsByTagName(t),o,u=[],a;for(var f=0,l=r.length;f<l;f+=1)i.push(new RegExp("(^|\\s)"+r[f]+"(\\s|$)"));for(var c=0,h=s.length;c<h;c+=1){o=s[c];a=!1;for(var p=0,d=i.length;p<d;p+=1){a=i[p].test(o.className);if(!a)break}a&&u.push(o)}return u};return t(e,n,r)};e=e.split(" ");var n=/\.[a-zA-Z0-9_-]*/,r=/#[a-zA-Z0-9_-]*/;if(!e[1]){e=e[0];if(r.test(e))return document.getElementById(e.replace("#",""));if(n.test(e)){var s=t(e.replace(".","")),o=[];for(i in s)(f()&&typeof s[i].getElementsByTagName=="object"||!f()&&typeof s[i].getElementsByTagName=="function")&&o.push(s[i]);return o[1]?o:o[0]}var s=document.getElementsByTagName(e);return s[1]?s:s[0]}var u=document,a;for(i in e)if(n.test(e[i])){e[i]=e[i].replace(".","");if(u[1]){a=[];for(s in u)if(f()&&typeof u[s].getElementsByTagName=="object"||!f()&&typeof u[s].getElementsByTagName=="function")for(x in t(e[i],"",u[s]))a.push(t(e[i],"",u[s])[x]);u=a.slice(0)}else{u=t(e[i],"",u);u[1]||(u=u[0])}}else if(r.test(e[i])){e[i]=e[i].replace("#","");u=document.getElementById(e[i])}else if(u[1]){a=[];for(s in u)if(f()&&typeof u[s].getElementsByTagName=="object"||!f()&&typeof u[s].getElementsByTagName=="function")for(x in u[s].getElementsByTagName(e[i]))a.push(u[s].getElementsByTagName(e[i])[x]);u=a.slice(0)}else{u=u.getElementsByTagName(e[i]);u[1]||(u=u[0])}return u};
+function getElem(e,c){function f(){if(navigator.userAgent.indexOf("MSIE 8")==-1&&navigator.userAgent.indexOf("MSIE 7")==-1&&navigator.userAgent.indexOf("MSIE 6")==-1)return!1;if(navigator.userAgent.indexOf("MSIE 8")>-1)return!0;if(navigator.userAgent.indexOf("MSIE 7")>-1)return!0;if(navigator.userAgent.indexOf("MSIE 6")>-1)return!0}var t=function(e,n,r){document.getElementsByClassName?t=function(e,t,n){n=n||document;var r=n.getElementsByClassName(e),i=t?new RegExp("\\b"+t+"\\b","i"):null,s=[],o;for(var u=0,a=r.length;u<a;u+=1){o=r[u];(!i||i.test(o.nodeName))&&s.push(o)}return s}:document.evaluate?t=function(e,t,n){t=t||"*";n=n||document;var r=e.split(" "),i="",s="http://www.w3.org/1999/xhtml",o=document.documentElement.namespaceURI===s?s:null,u=[],a,f;for(var l=0,c=r.length;l<c;l+=1)i+="[contains(concat(' ', @class, ' '), ' "+r[l]+" ')]";try{a=document.evaluate(".//"+t+i,n,o,0,null)}catch(h){a=document.evaluate(".//"+t+i,n,null,0,null)}while(f=a.iterateNext())u.push(f);return u}:t=function(e,t,n){t=t||"*";n=n||document;var r=e.split(" "),i=[],s=t==="*"&&n.all?n.all:n.getElementsByTagName(t),o,u=[],a;for(var f=0,l=r.length;f<l;f+=1)i.push(new RegExp("(^|\\s)"+r[f]+"(\\s|$)"));for(var c=0,h=s.length;c<h;c+=1){o=s[c];a=!1;for(var p=0,d=i.length;p<d;p+=1){a=i[p].test(o.className);if(!a)break}a&&u.push(o)}return u};return t(e,n,r)};e=e.split(" ");var n=/\.[a-zA-Z0-9_-]*/,r=/#[a-zA-Z0-9_-]*/;if(!e[1]){e=e[0];if(r.test(e))return document.getElementById(e.replace("#",""));if(n.test(e)){var s=t(e.replace(".","")),o=[];for(i in s)(f()&&typeof s[i].getElementsByTagName=="object"||!f()&&typeof s[i].getElementsByTagName=="function")&&o.push(s[i]);return o[1]?o:o[0]}var s=document.getElementsByTagName(e);return s[1]?s:s[0]}var u=document,a;for(i in e)if(n.test(e[i])){e[i]=e[i].replace(".","");if(u[1]){a=[];for(s in u)if(f()&&typeof u[s].getElementsByTagName=="object"||!f()&&typeof u[s].getElementsByTagName=="function")for(x in t(e[i],"",u[s]))a.push(t(e[i],"",u[s])[x]);u=a.slice(0)}else{u=t(e[i],"",u);u[1]||(u=u[0])}}else if(r.test(e[i])){e[i]=e[i].replace("#","");u=document.getElementById(e[i])}else if(u[1]){a=[];for(s in u)if(f()&&typeof u[s].getElementsByTagName=="object"||!f()&&typeof u[s].getElementsByTagName=="function")for(x in u[s].getElementsByTagName(e[i]))a.push(u[s].getElementsByTagName(e[i])[x]);u=a.slice(0)}else{u=u.getElementsByTagName(e[i]);u[1]||(u=u[0])}return u};
 
 /*
+ *
  * ******************
  * *** Aesthetics ***
  * ******************
  * - includes media query polyfills, html5 placeholders polyfill, dropdowns, tooltips, modal (lightbox), etc
- * - Dropdowns and onwards require jQuery
+ * - tooltips and onwards require jQuery
  *
-*/
+ */
 
 /* 
- * Resizing Browser - includes media query polyfill for ie
- * For the main menu when screen size < 768px
- * @nav is main menu navigation
- * @navdrop is button for dropping down nav when screen viewport < 768px
-*/
+ *  Non-jQuery - includes Resizing Browser javascript, media queries polyfill, html5 placeholders polyfill, dropdown clicking
+ */
+
+ /* Placeholders.js - a raw-js ie placeholders solution
+  * Made by Gurudayal Khalsa
+ */
+ function placeholders(e){var e;typeof e=="undefined"&&(e="#888888");if("placeholder"in document.createElement("input"))return;t(document.getElementsByTagName("input"));t(document.getElementsByTagName("textarea"));function t(t){var n,r,i,s,o,u;for(n=0,r=t.length;n<r;n++){i=t[n].style.color;if(t[n].nodeName=="TEXTAREA"||t[n].type==="text"){t[n].value=t[n].placeholder;t[n].style.color=e;t[n].onfocus=function(){if(this.value===this.placeholder){this.value="";this.style.color=i}};t[n].onblur=function(){if(this.value===""){this.value=this.placeholder;this.style.color=e}}}else if(t[n].type==="password"){t[n].value=t[n].placeholder;s=t[n].placeholder;o=document.createElement("input");u=t[n].parentNode.cloneNode();o.type="text";o.style.color=e;o.placeholder=t[n].placeholder;o.value=o.placeholder;t[n].parentNode.replaceChild(o,t[n]);a(t[n]);function a(t){t.onfocus=function(){var t,n;if(this.value===this.placeholder){value=this.value;t=document.createElement("input");t.type="password";t.placeholder=value;t.value="";t.name="pw";t.style.letterSpacing="3px";t.style.color=i;this.parentNode.replaceChild(t,this);setTimeout(function(){t.focus()},10);n=document.getElementsByName("pw")[0];n.removeAttribute("name");n.onblur=function(){var t,n;if(this.value===""){t=document.createElement("input");t.type="text";t.placeholder=value;t.value=value;t.name="pw";t.style.color=e;this.parentNode.replaceChild(t,this);n=document.getElementsByName("pw")[0];n.removeAttribute("name");a(n)}}}}}}}}};
+
+ placeholders();
 
 (function(){
     
@@ -62,6 +67,70 @@ function getElem(e){function f(){if(navigator.userAgent.indexOf("MSIE 8")==-1&&n
     window.matchMedia=window.matchMedia||function(a){"use strict";var c,d=a.documentElement,e=d.firstElementChild||d.firstChild,f=a.createElement("body"),g=a.createElement("div");return g.id="mq-test-1",g.style.cssText="position:absolute;top:-100em",f.style.background="none",f.appendChild(g),function(a){return g.innerHTML='&shy;<style media="'+a+'"> #mq-test-1 { width: 42px; }</style>',d.insertBefore(f,e),c=42===g.offsetWidth,d.removeChild(f),{matches:c,media:a}}}(document);
     /*! Respond.js v1.1.0: min/max-width media query polyfill. (c) Scott Jehl. MIT/GPLv2 Lic. j.mp/respondjs  */
     (function(a){"use strict";function x(){u(!0)}var b={};if(a.respond=b,b.update=function(){},b.mediaQueriesSupported=a.matchMedia&&a.matchMedia("only all").matches,!b.mediaQueriesSupported){var q,r,t,c=a.document,d=c.documentElement,e=[],f=[],g=[],h={},i=30,j=c.getElementsByTagName("head")[0]||d,k=c.getElementsByTagName("base")[0],l=j.getElementsByTagName("link"),m=[],n=function(){for(var b=0;l.length>b;b++){var c=l[b],d=c.href,e=c.media,f=c.rel&&"stylesheet"===c.rel.toLowerCase();d&&f&&!h[d]&&(c.styleSheet&&c.styleSheet.rawCssText?(p(c.styleSheet.rawCssText,d,e),h[d]=!0):(!/^([a-zA-Z:]*\/\/)/.test(d)&&!k||d.replace(RegExp.$1,"").split("/")[0]===a.location.host)&&m.push({href:d,media:e}))}o()},o=function(){if(m.length){var b=m.shift();v(b.href,function(c){p(c,b.href,b.media),h[b.href]=!0,a.setTimeout(function(){o()},0)})}},p=function(a,b,c){var d=a.match(/@media[^\{]+\{([^\{\}]*\{[^\}\{]*\})+/gi),g=d&&d.length||0;b=b.substring(0,b.lastIndexOf("/"));var h=function(a){return a.replace(/(url\()['"]?([^\/\)'"][^:\)'"]+)['"]?(\))/g,"$1"+b+"$2$3")},i=!g&&c;b.length&&(b+="/"),i&&(g=1);for(var j=0;g>j;j++){var k,l,m,n;i?(k=c,f.push(h(a))):(k=d[j].match(/@media *([^\{]+)\{([\S\s]+?)$/)&&RegExp.$1,f.push(RegExp.$2&&h(RegExp.$2))),m=k.split(","),n=m.length;for(var o=0;n>o;o++)l=m[o],e.push({media:l.split("(")[0].match(/(only\s+)?([a-zA-Z]+)\s?/)&&RegExp.$2||"all",rules:f.length-1,hasquery:l.indexOf("(")>-1,minw:l.match(/\(\s*min\-width\s*:\s*(\s*[0-9\.]+)(px|em)\s*\)/)&&parseFloat(RegExp.$1)+(RegExp.$2||""),maxw:l.match(/\(\s*max\-width\s*:\s*(\s*[0-9\.]+)(px|em)\s*\)/)&&parseFloat(RegExp.$1)+(RegExp.$2||"")})}u()},s=function(){var a,b=c.createElement("div"),e=c.body,f=!1;return b.style.cssText="position:absolute;font-size:1em;width:1em",e||(e=f=c.createElement("body"),e.style.background="none"),e.appendChild(b),d.insertBefore(e,d.firstChild),a=b.offsetWidth,f?d.removeChild(e):e.removeChild(b),a=t=parseFloat(a)},u=function(b){var h="clientWidth",k=d[h],m="CSS1Compat"===c.compatMode&&k||c.body[h]||k,n={},o=l[l.length-1],p=(new Date).getTime();if(b&&q&&i>p-q)return a.clearTimeout(r),r=a.setTimeout(u,i),void 0;q=p;for(var v in e)if(e.hasOwnProperty(v)){var w=e[v],x=w.minw,y=w.maxw,z=null===x,A=null===y,B="em";x&&(x=parseFloat(x)*(x.indexOf(B)>-1?t||s():1)),y&&(y=parseFloat(y)*(y.indexOf(B)>-1?t||s():1)),w.hasquery&&(z&&A||!(z||m>=x)||!(A||y>=m))||(n[w.media]||(n[w.media]=[]),n[w.media].push(f[w.rules]))}for(var C in g)g.hasOwnProperty(C)&&g[C]&&g[C].parentNode===j&&j.removeChild(g[C]);for(var D in n)if(n.hasOwnProperty(D)){var E=c.createElement("style"),F=n[D].join("\n");E.type="text/css",E.media=D,j.insertBefore(E,o.nextSibling),E.styleSheet?E.styleSheet.cssText=F:E.appendChild(c.createTextNode(F)),g.push(E)}},v=function(a,b){var c=w();c&&(c.open("GET",a,!0),c.onreadystatechange=function(){4!==c.readyState||200!==c.status&&304!==c.status||b(c.responseText)},4!==c.readyState&&c.send(null))},w=function(){var b=!1;try{b=new a.XMLHttpRequest}catch(c){b=new a.ActiveXObject("Microsoft.XMLHTTP")}return function(){return b}}();n(),b.update=n,a.addEventListener?a.addEventListener("resize",x,!1):a.attachEvent&&a.attachEvent("onresize",x)}})(this);
+
+    function hasClass(elem, className) {
+        return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+    }
+
+    function removeClass(elem, className) {
+        var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+        if (hasClass(elem, className)) {
+            while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+                newClass = newClass.replace(' ' + className + ' ', ' ');
+            }
+            elem.className = newClass.replace(/^\s+|\s+$/g, '');
+        }
+    }
+
+    function toggleClass(elem, className) {
+        var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ' ) + ' ';
+        if (hasClass(elem, className)) {
+            while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+                newClass = newClass.replace( ' ' + className + ' ' , ' ' );
+            }
+            elem.className = newClass.replace(/^\s+|\s+$/g, '');
+        } else {
+            elem.className += ' ' + className;
+        }
+    }
+
+    function cancelBubble(e) {
+     var evt = e ? e:window.event;
+     if (evt.stopPropagation)    evt.stopPropagation();
+     if (evt.cancelBubble!=null) evt.cancelBubble = true;
+    }
+
+    /*
+     * *********************
+     * ***** Dropdowns *****
+     * *********************
+     * - dropdown must have class names dropdown and click
+    */ 
+
+    //for the input element ".dropdown.click", enables element's subchild "ul" element to be not hidden when element is clicked
+
+    var dropdowns = getElem(".dropdown");
+
+    for (i in dropdowns)
+    {
+        if(hasClass(dropdowns[i],"click"))
+        {
+
+            dropdowns[i].onclick=function(event)
+            {
+                toggleClass(this, "open");
+                cancelBubble(event);
+            };
+
+            window.onclick=function()
+            {
+                for (i in dropdowns)
+                {
+                    removeClass(dropdowns[i], "open");
+                }
+            };
+        }
+    }
 })();
 
 // The following code is dependent on jQuery
@@ -71,68 +140,65 @@ if(typeof window.jQuery !== "undefined")
 
 /*
  * *********************
- * ***** Dropdowns *****
- * *********************
- * - dropdown must have class names dropdown and click
-*/ 
-
-//for the input jQuery element ".dropdown.click", enables element's subchild "ul" element to be not hidden when element is clicked
-
-var dropdowns = $(".dropdown.click");
-
-dropdowns.click(function(event)
-{
-    var dropdown = $(this);
-    var offset = dropdown.offset();
-    dropdown.toggleClass("open");
-    event.stopPropagation();
-});
-$(window).click(function()
-{
-    dropdowns.removeClass("open");
-});
-
-/*
- * *********************
  * ***** Tooltips ******
  * *********************
  * - to add a tooltip to anything, in html write data-tooltip="Nameoftooltip" in html. e.g. <a data-tooltip="this is a link">Hello</a>
-*/ 
+ */ 
 
-$("[data-tooltip]").mouseenter(function()
+
+(function(){
+
+var tooltipNames = [];
+
+$("[data-tooltip]").each(function(i,element)
 {
-    var e = $(this);
-    var offset = e.offset();
+    var e = $(element);
+    tooltipNames[i] = e.attr("data-tooltip");
 
-    var tooltip = $(document.createElement("div"))
-                  .addClass("tooltip")
-                  .append(e.attr("data-tooltip"))
-                  .appendTo(e)
-                  .hide();
-    
-    setTimeout(function()
+    e.mouseenter(function()
     {
-        tooltip.fadeIn(200,function()
+        var offset = e.offset();
+
+        var tooltip = $(document.createElement("div"))
+                      .addClass("tooltip")
+                      .append(tooltipNames[i])
+                      .css({ "left":offset.left, "top":offset.top+e.height() })
+                      .appendTo(document.body)
+                      .hide();
+
+        setTimeout(function()
         {
-            $(this).css("opacity",0.9);
-        });
-    },300);
+            tooltip.fadeIn(200,function()
+            {
+                $(this).css("opacity",0.9);
+            })
+        },300);
+    });
+
+    e.mouseleave(function()
+    {
+        $(".tooltip").each(function(j,e)
+        {
+            e = $(e);
+            if(e.text()==tooltipNames[i]) 
+            {
+                setTimeout(function()
+                {
+                    e.fadeOut(200,function(){$(this).remove()});
+                },  100);
+            }
+        });        
+    });
 });
 
-$("[data-tooltip]").mouseleave(function()
-{
-    setTimeout(function()
-    {
-        $(".tooltip").fadeOut(200,function(){$(this).remove()});
-    },  100);
-});
+})();
 
 /*
  * *********************
  * ****** Buttons ******
  * *********************
  * - 
-*/ 
+ */ 
 var radio = $("[data-toggle='button-radio'] .button");
 radio.click(function()
 {
@@ -150,7 +216,7 @@ radio.click(function()
  * ****** Modals *******
  * *********************
  * - 
-*/ 
+ */ 
 
 })(window.jQuery);
 
